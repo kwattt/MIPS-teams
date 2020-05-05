a2ac4e117fb0b2c9b44d740ceb55f4c5d728df19
kwattt <jorgeglez1616@gmail.com>
2020-05-05 02:01:07 -0500

    fucniones de binario

--- decodificador/Decodificador.py
+++ decodificador/Decodificador.py
@@ -1,2 +1,18 @@
 import re 
+import os # For testing
+
 from Diccionarios import * 
+
+
+## Funciones para manejo de binarios
+
+def fullbin(x, c):
+    return ("0"*(c-len(str(x))))+str(x)
+
+def dec_to_bin(x):
+    return int(bin(x)[2:])
+
+def dbin(x,c):
+    if(x == -1): return "x"*c
+    return fullbin(dec_to_bin(x), c)
+
