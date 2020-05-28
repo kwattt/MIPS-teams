9ea0b98830ac48ba7a3b50f738417936e2631419
kwattt <jorgeglez1616@gmail.com>
2020-05-28 03:09:12 -0500

    triangulo.asm done

--- decodificador/asm/triangulo.asm
+++ decodificador/asm/triangulo.asm
@@ -1,20 +1,10 @@
 .main:
-
-    li $t0 10 # lado 1
-    li $t1 20 # lado 2
-    li $t2 10 # lado 3
-
-    # 0 == 1 && 0 == 2 -> equilatero
-
-    # equilatero <- 
+    li $t0 2 # lado 1
+    li $t1 35 # lado 2
+    li $t2 30 # lado 3
     sub $t4 $t0 $t1
-    sub $t5 $t0 $t2 
-    # si amas son 0 entonces equilatero
-    
+    sub $t5 $t0 $t2
     beq $t4 $zero .f1 
-
-    # si no son iguales entonces probemos un isoceles
-    # a==b||a==c||b==c  
 
     .iso:
 
@@ -29,6 +19,7 @@
 
     .f1:   
     beq $t5 $zero .equilatero 
+
     j .iso 
 
 .equilatero:
@@ -41,11 +32,6 @@
 
 .escaleno:
     li $t3 3  
-    j .end 
-
 
 .end:
     sw $t3 0($zero) # guardar tipo de triangulo
-    # 1 -> equilatero
-    # 2 -> isoceles
-    # 3 -> escaleno
